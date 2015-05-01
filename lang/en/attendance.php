9b30bc685ebc657d246c9039e8e540d3f190706b
ggarreton <german.garreton@gmail.com>
2015-05-01 18:34:36 -0400

    agregando plugin

---
+++ lang/en/attendance.php
@@ -0,0 +1,39 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * English strings for attendance
+ *
+ * You can have a rather longer description of the file as well,
+ * if you like, and it can span multiple lines.
+ *
+ * @package    mod_attendance
+ * @copyright  2015 Your Name
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'attendance';
+$string['modulenameplural'] = 'attendances';
+$string['modulename_help'] = 'Use the attendance module for... | The attendance module allows...';
+$string['attendancefieldset'] = 'Custom example fieldset';
+$string['attendancename'] = 'attendance name';
+$string['attendancename_help'] = 'This is the content of the help tooltip associated with the attendancename field. Markdown syntax is supported.';
+$string['attendance'] = 'attendance';
+$string['pluginadministration'] = 'attendance administration';
+$string['pluginname'] = 'attendance';
