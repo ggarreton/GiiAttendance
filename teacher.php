bc6f4bd4e1fe162dee7427f384664ddf6241ccaf
ggarreton <german.garreton@gmail.com>
2015-05-19 00:22:07 -0400

    arreglando error de tipeo

--- teacher.php
+++ teacher.php
@@ -96,7 +96,7 @@ echo'<ul class="nav nav-tabs">
 <div class="tab-pane" id="tab1">';
 echo '<ul class="nav nav-pills nav-stacked">
   <li role="presentation"><a href="passattendance.php?id='.$id.'">Take Attendance</a></li>
-  <li role="presentation"><a href="teacher.php?id='.$id.'">Students Mark Assistance</a></li>
+  <li role="presentation"><a href="teacher.php?id='.$id.'">Students Mark Attendance</a></li>
 </ul>';
 echo'</div>
 <div class="tab-pane active" id="tab2">';
