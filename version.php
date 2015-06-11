f90229b6d643976783fc11490f9d5feb93e11c90
cami2708 <cami2708@yahoo.es>
2015-06-11 11:43:34 -0300

    Actualizando version hacia RC

--- version.php
+++ version.php
@@ -28,9 +28,9 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component 		= 'mod_attendance';
-$plugin->version 		= '2015051600';
+$plugin->version 		= '2015061100';
 $plugin->release 		= 'v0.0.1';
 $plugin->requires 		= 2014051200;
-$plugin->maturity 		= MATURITY_ALPHA;
+$plugin->maturity 		= MATURITY_RC;
 $plugin->cron 			= 0;
 $plugin->dependencies 	= array();
